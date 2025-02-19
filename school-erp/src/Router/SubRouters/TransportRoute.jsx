65dcd48b90f6d2305a032ad94726f84b28276960
JKGlobalItSolutions <jkglobalitsolutionsweb1@gmail.com>
2025-02-19 12:11:40 +0530

    Backend
    Worked in the Place Setup page and implemented the Bus  Route setup and the Conductor and the Driver setup and also Completed the Place Setup compleeted successfully fetching from the firestore database and also Worked in the Sidebar for the updation And the Datas were stored in the Transport collection  inside their respective sub collection inside .

--- school-erp/src/Router/SubRouters/TransportRoute.jsx
+++ school-erp/src/Router/SubRouters/TransportRoute.jsx
@@ -10,6 +10,7 @@ import PeriodBusCollection from '../../pages/Transport Pages/PeriodBusCollection
 import BusBalanceReport from '../../pages/Transport Pages/BusBalanceReport';
 import PlacewiseReport from '../../pages/Transport Pages/Placewisereport';
 import BusVanFeeHeadSetup from '../../pages/Transport Pages/BusVanFee';
+import DriverConductorRouteSetup from '../../pages/Transport Pages/DriverConductorRouteSetup';
 
 function TransportRoute() {
   return (
@@ -22,6 +23,7 @@ function TransportRoute() {
       <Route path="period-bus-collection" element={< PeriodBusCollection/>} />    
       <Route path="bus-balance-report" element={< BusBalanceReport/>} />    
       <Route path="place-wise-report" element={< PlacewiseReport/>} />    
+      <Route path="driver-conductor-setup" element={< DriverConductorRouteSetup/>} />    
     </Routes>
   );
 }
